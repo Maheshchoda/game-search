296ffa2c9c944e41283ed8cbdf65f36c350b84a0
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-03 12:27:51 +0530

    Add hover effect to list of games

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,15 @@ interface Props {
 }
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow="hidden">
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      transition="transform 0.3s ease, box-shadow 0.3s ease, width 0.3s ease, height 0.3s ease"
+      _hover={{
+        transform: "scale(1.05)",
+        boxShadow: "lg",
+      }}
+    >
       {children}
     </Box>
   );
