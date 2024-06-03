bd88d6f456f3775c0f0f406e0a5a686a9fe20816
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-03 12:08:21 +0530

    setup Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
-import theme from "./theme.ts";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./router.tsx";
+import theme from "./theme.ts";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
